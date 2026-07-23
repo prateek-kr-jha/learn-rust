bf9b33e8749fdd7b560ff5469efdd512d7e6e659
prateek <prateek.jha@yulu.bike>
2026-07-23 23:46:06 +0530

    added datda type

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -7,6 +7,8 @@ fn main() {
         let x = x * 2;
         println!("The value of x in the inner scope is: {x}");
     }
-
+    let tup: (i32, f64, u8) = (-250, 6.4, 1);
+    // let (x, y, z) = tup;
+    println!("{} {} {}", tup.0, tup.1, tup.2);
     println!("The value of x is: {x}");
 }
