5e4518674068dbf5a495fd9f1f2962ba14569c93
prateek <prateek.jha@yulu.bike>
2026-07-25 12:05:10 +0530

    added new method

--- method/src/main.rs
+++ method/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(5, 'X');
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x: i32, unit_label: char) {
+    println!("the value of x is {x} {unit_label}");
 }
