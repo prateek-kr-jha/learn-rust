37fe2083d99e2b642d70ae5e76d9d1e304249cd5
prateek <prateek.jha@yulu.bike>
2026-07-29 23:13:50 +0530

    added for loops and while loops

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,22 +1,29 @@
 fn main() {
-    let mut count = 0;
+    let mut number = 3;
 
-    'counting_up: loop {
-        println!("Count = {count}");
-        let mut remaining = 10;
+    while number != 0 {
+        println!("{number}!");
 
-        loop {
-            println!("remaining = {remaining}");
-
-            if remaining == 9 {
-                break;
-            }
-            if count == 2 {
-                break 'counting_up;
-            }
-            remaining -= 1;
-        }
-        count += 1;
+        number -= 1;
     }
-    println!("End count = {count}");
+
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 5 {
+        println!("The value is: {}", a[index]);
+        index += 1;
+    }
+
+    for element in a {
+        println!("The for value is: {element}");
+    }
+
+    for number in (1..4).rev() {
+        println!("{number}!");
+    }
+
+    println!("LIFTOFF!!!");
 }
